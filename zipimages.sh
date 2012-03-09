e5894b7057c456f21f725ea48d7c0902f54bf170
Matt Mastracci <matthew@mastracci.com>
2012-03-08 22:34:03 -0700

    Change batch size

--- zipimages.sh
+++ zipimages.sh
@@ -1,5 +1,6 @@
 cd static/images/
-for N in {0..100}
+BATCH_SIZE=1000
+for N in {0..200}
 do
-	zip -9 /tmp/$[N*2500].zip `seq -f %g.png $[N*2500] $[(N+1)*2500-1]` | xargs
+	zip -9 /tmp/$[N*BATCH_SIZE].zip `seq -f %g.png $[N*BATCH_SIZE] $[(N+1)*BATCH_SIZE-1]` | xargs
 done
